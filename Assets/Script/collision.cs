c13118edbc5ca4a9d355b64c46e71a00c3068585
aydogm_i <ismaydogmus@gmail.com>
2015-07-06 11:17:26 +0200

    the collision between spades and player

---
+++ Assets/Script/collision.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collision : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("COLLISION !!!");
+		if (other.tag == "Player") {
+			if (this.transform.position.x < transform.position.x){
+				Debug.Log ("left");
+			}
+			else {
+				Debug.Log("right");
+			}
+		}
+	}	
+}
