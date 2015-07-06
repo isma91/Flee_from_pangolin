b2db795320170f06b9cf187149ff13ba17b1253a
aydogm_i <ismaydogmus@gmail.com>
2015-07-06 11:16:11 +0200

    the player jump if you touch or click the ground script

---
+++ Assets/Script/jumpAllTheGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jumpAllTheGame : MonoBehaviour {
+	public GameObject joueur;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown () {
+		if(joueur.transform.position.y < 10){
+			joueur.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+		} else {
+			joueur.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 0), ForceMode2D.Impulse);
+		}
+	}
+}
