4846b3d9c513fb03253928228c4d43b7e1f6b6a7
aydogm_i <ismaydogmus@gmail.com>
2015-07-06 11:17:07 +0200

    the killer script

---
+++ Assets/Script/deleteSol.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deleteSol : MonoBehaviour {
+	public GameObject camera;
+	void OnTriggerEnter2D (Collider2D allObject) {
+		if (camera.transform.position.x >= 1) {
+			if (camera.transform.position.x % 4 < 2 || camera.transform.position.x % 4 > 2) {
+				if (allObject.transform.parent) {
+					Destroy (allObject.gameObject.transform.parent.gameObject);
+				} else {
+					Destroy (allObject.gameObject);
+				}
+			}
+		}
+	}
+}
