0a977587614632626cfb38f19c0204a5e0c6f9c2
aydogm_i <ismaydogmus@gmail.com>
2015-07-06 11:16:59 +0200

    the follow camera scrip

---
+++ Assets/Script/FollowPlayerCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayerCamera : MonoBehaviour {
+	public GameObject joueur;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (joueur.transform.position.x, 3, joueur.transform.position.z - 1);
+	}
+	
+}
